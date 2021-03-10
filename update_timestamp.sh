bea532547c3e4eb323a85495adf7b438050b2b3c
tomo <tomo14142356@gmail.com>
2021-03-10 23:33:49 +0900

    feat: add scripts for git
    gitの最終コミットの日時にファイルの更新日を復元するスクリプトの追加

---
+++ update_timestamp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git_files=$(git ls-files)
+
+for FILE in ${git_files}; do
+  TIME=$(git log --pretty=format:%ci -n1 "$FILE")
+  printf '%s\t%s\n' "$TIME" "$FILE"
+  STAMP=$(date -d "$TIME" +"%y%m%d%H%M.%S")
+  touch -t "$STAMP" "$FILE"
+done
