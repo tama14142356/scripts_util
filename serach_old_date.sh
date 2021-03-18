818bd5fdbe1bd63036c068de6efd79a8a4287cdb
tomo <tomo14142356@gmail.com>
2021-03-18 15:22:25 +0900

    fix: change format for date command
    iso-8601形式と少し違っていたので、iso-8601形式になるように変更

--- serach_old_date.sh
+++ serach_old_date.sh
@@ -4,6 +4,6 @@ old_file_name=$(ls -1atrd $(find "$(pwd)" -type f) | head -1)
 # shellcheck disable=SC2012,SC2046
 old_directory_name=$(ls -1atrd $(find "$(pwd)" -type d) | head -1)
 echo "$old_directory_name"
-date -r "$old_directory_name" '+%Y-%m-%dT%H:%M:%S%z'
+date -r "$old_directory_name" '+%Y-%m-%dT%H:%M:%S%:z'
 echo "$old_file_name"
-date -r "$old_file_name" '+%Y-%m-%dT%H:%M:%S%z'
+date -r "$old_file_name" '+%Y-%m-%dT%H:%M:%S%:z'
