323ea88812c0812ae8e340ebe345d0c87092c6bc
tomo <tomo14142356@gmail.com>
2021-08-13 19:06:10 +0900

    Add inf loop scripts

--- for_process/test_loop.sh
+++ for_process/test_loop.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 #引数の数だけループさせる
-for i in $(seq 1 ${#}); do
-    echo 'looping: '
-    echo "$i"
-    echo "${1}" #第一引数を表示
-    shift       #shift
+while true
+do
+  sleep 1
+  date_str=$(date)
+  # echo "$date_str"
 done
+# for i in $(seq 1 ${#}); do
+#     echo 'looping: '
+#     echo "$i"
+#     echo "${1}" #第一引数を表示
+#     shift       #shift
+# done
 # background_process=$(ps axo user,pid,args | grep "$1" | grep -v grep | grep -v bash | awk '{print $2}')
 # set -f -- "$background_process"
 # #引数の数だけループさせる
